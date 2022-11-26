d8278a1f8e2529e01c83a403c32efd35c643d476
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-11-26 11:14:22 -0600

    Initial Commit

---
+++ AnyLayoutDemo/AnyLayoutDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyLayoutDemoApp.swift
+//  AnyLayoutDemo
+//
+//  Created by Mark Volkmann on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct AnyLayoutDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
